8ece7746ca328ea23eead2eb17212c57cead0395
KaranJayakumar <117019183+KaranJayakumar@users.noreply.github.com>
2024-04-24 05:03:18 +1000

    implemented login endpoint

--- backend/messager/src/main/java/com/karan/messager/controller/AuthController.java
+++ backend/messager/src/main/java/com/karan/messager/controller/AuthController.java
@@ -4,12 +4,17 @@ import com.karan.messager.config.TokenProvider;
 import com.karan.messager.exception.UserException;
 import com.karan.messager.modal.User;
 import com.karan.messager.repository.UserRepository;
+import com.karan.messager.request.LoginRequest;
 import com.karan.messager.response.AuthResponse;
+import com.karan.messager.service.CustomUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
@@ -20,10 +25,12 @@ public class AuthController {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     private TokenProvider tokenProvider;
-    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenProvider tokenProvider){
+    private CustomUserService customUserService;
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenProvider tokenProvider, CustomUserService customUserService){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.tokenProvider = tokenProvider;
+        this.customUserService = customUserService;
     }
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException {
@@ -43,5 +50,24 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = tokenProvider.generateToken(authentication);
         return new ResponseEntity<AuthResponse>(new AuthResponse(jwt, true), HttpStatus.ACCEPTED);
+    }
+    @PostMapping("login")
+    public ResponseEntity<AuthResponse> loginHandler(@RequestBody LoginRequest loginRequest){
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        Authentication authentication = authenticate(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.generateToken(authentication);
+        return new ResponseEntity<AuthResponse>(new AuthResponse(jwt, true), HttpStatus.ACCEPTED);
+    }
+    public Authentication authenticate(String email, String password){
+        UserDetails userDetails = customUserService.loadUserByUsername(email);
+        if(userDetails == null){
+            throw new BadCredentialsException("Invalid email or password");
+        }
+        if(!passwordEncoder.encode(password).matches(userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid email or password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
