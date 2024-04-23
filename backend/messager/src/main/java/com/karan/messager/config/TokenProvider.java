160138fb2a623437424a8885c53069c922412549
KaranJayakumar <117019183+KaranJayakumar@users.noreply.github.com>
2024-04-24 02:51:30 +1000

    feat:Began building tokenProvider class

--- backend/messager/src/main/java/com/karan/messager/config/TokenProvider.java
+++ backend/messager/src/main/java/com/karan/messager/config/TokenProvider.java
@@ -1,4 +1,29 @@
 package com.karan.messager.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class TokenProvider {
+    SecretKey key = Keys.hmacShaKeyFor(JWTConstant.SECRET_KEY.getBytes());
+    public String generateToken(Authentication auth) {
+        return Jwts.builder().setIssuer("Karan's code")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 86400000))
+                .claim("email", auth.getName())
+                .signWith(key)
+                .compact();
+
+    }
+    public String getEmailFromToken(String jwt){
+        jwt = jwt.substring(7);
+        Claims claim = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+        return String.valueOf(claim.get("email"));
+    }
 }
