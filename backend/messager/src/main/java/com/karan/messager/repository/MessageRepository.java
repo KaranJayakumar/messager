81d3161dd3756368b86e06c19714f4df8eda055c
KaranJayakumar <117019183+KaranJayakumar@users.noreply.github.com>
2024-04-24 15:43:15 +1000

    implemented MessageRepository

---
+++ backend/messager/src/main/java/com/karan/messager/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.karan.messager.repository;
+
+import com.karan.messager.modal.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query("SELECT m from Message m JOIN m.chatServer cs WHERE c.id = :chatId")
+    public List<Message> findMessagesWithChatId(@Param("chatId") Integer chatId);
+}
